47d7e4f42d14985108ab8d99c0d3a62e0b60b9be
Fahad Mahmood <Fahad@Kashifs-MacBook-Pro.local>
2015-07-10 17:22:50 -0500

    16th

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -20,9 +20,6 @@ ON_HEROKU = os.environ.get('ON_HEROKU')
 
 
 
-
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -38,42 +35,42 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'core',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'core',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'coffeedapp.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(MAIN_DIR, 'templates')],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'coffeedapp.wsgi.application'
@@ -90,27 +87,27 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 #}
 # Parse database configuration from $DATABASE_URL
 
-if ON_HEROKU == True:
+if ON_HEROKU == 1:
 
-    # Parse database configuration from $DATABASE_URL
+	# Parse database configuration from $DATABASE_URL
 
-    import dj_database_url
+	import dj_database_url
 
-    DATABASES['default'] = dj_database_url.config()
+	DATABASES['default'] = dj_database_url.config()
 
 else: 
 
-    DATABASES = {
+	DATABASES = {
 
-        'default': {
+		'default': {
 
-            'ENGINE': 'django.db.backends.sqlite3',
+			'ENGINE': 'django.db.backends.sqlite3',
 
-            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+			'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
 
-        }
+		}
 
-    }
+	}
 
 
 
@@ -145,8 +142,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATES_DIRS = (
-    os.path.join(MAIN_DIR, 'templates'),
-    )
+	os.path.join(MAIN_DIR, 'templates'),
+	)
 
 STATICFILES_DIRS = (
 
